915184e25bb92006a4dc1b8e3791d05d97820b6b
Kelly Z <kzimmerman@energy-hub.net>
2017-04-03 19:24:17 -0400

    initial commit

---
+++ src/project4yourself.java
@@ -0,0 +1,14 @@
+
+public class project4yourself {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is me");
+	}
+	//Jesse Jang
+	//Fight Club
+	//This is the only program I know how to write and run
+//There's a lot more I need to learn
+	//Java the hutt
+	//Java is difficult
+}
